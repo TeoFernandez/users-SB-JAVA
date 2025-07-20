f61390717cb68c4d8cc350d68579a15cf7ffa527
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-07-19 23:00:29 -0300

    Entity "User.java"

---
+++ user-crud/src/main/java/com/example/user_crud/entity/User.java
@@ -0,0 +1,20 @@
+package com.example.user_crud.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+}
